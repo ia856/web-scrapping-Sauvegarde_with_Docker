9d02eb333117f08d67dd01b0d64cd99f7e494002
ia856 <diallomouhawa97@gmail.com>
2025-01-29 00:11:57 +0100

    Modify the crawler to to get all Jobs Titles, Date of published and Resume

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request, urlopen
+from pandas as pd
 
 url="https://www.sfds.asso.fr/fr/n/506-consulter_les_offres_demploi/?jedu=MASTER&jcon=&jp=3"
 page=urlopen(url)
@@ -7,5 +8,43 @@ html=bs(page,'html.parser')
 #print(html)
 titre=html.find_all('td',{'class':'jobLabel'})
 
-for element in titre :
-    print (element.get_text())
+##for element in titre :
+    ##print (element.get_text())
+
+
+allJobs = html.find_all('div', class_='job') 
+titre_offre=[]
+date_offre=[]
+resume_offre=[]
+
+for job in allJobs:
+    for titles in job.find_all('td',{'class':'jobLabel'}):
+        title=titles.get_text()
+        titre_offre.append(title)
+
+    datepubs = job.find_all('td',{'class':'jobPublished'})
+    date=datepubs[0].get_text()
+    date_offre.append(date)
+
+    for resumes in job.find_all('td',{'class':'jobResume'}):
+        resume=resumes.get_text()
+        resume_offre.append(resume)
+
+    
+# Print all Titles Jobs
+print(titre_offre)
+
+# Print all Date Pub
+print(date_offre)
+
+# Print all Resume
+print(resume_offre)
+
+
+dfJobs=pd.DataFrame({
+    'TITRE_OFFRE':titre_offre,
+    'DATE_OFFRE':date_offre,
+    'RESUME_OFFRE':resume_offre
+
+})
+dfJobs.to_csv('jobs.csv')
