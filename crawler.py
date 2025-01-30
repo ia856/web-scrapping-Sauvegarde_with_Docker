521beaf5a9c711667a7c5ceaaee139ad43448837
ia856 <diallomouhawa97@gmail.com>
2025-01-30 18:55:14 +0100

    Ajout de pymysql dans requirements

--- crawler.py
+++ crawler.py
@@ -52,7 +52,7 @@ dfJobs=pd.DataFrame({
 # create sqlalchemy engine
 user='root'
 passw='supersecret'
-host='localhost' # either localhost or ip e.g? '172.17.0.2' or hostname adress
+host='host.docker.internal' # either localhost or ip e.g? '172.17.0.2' or hostname adress
 port=3307
 database='jobs_scraping'
 
